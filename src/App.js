462765cf76be801dedf379108f16f229e712cca7
pgvda <dilshanariyarathna1999@gmail.com>
2024-12-28 23:52:10 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './view/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginPage/>
     </div>
   );
 }
